fa52984537512abf5981af59a7b54ca02e33f248
Sander Mertens <sander.mertens8@gmail.com>
2026-02-25 23:41:17 -0800

    Fix bake setup

--- src/env/environment.c
+++ src/env/environment.c
@@ -1029,21 +1029,64 @@ int bake_environment_cleanup(bake_context_t *ctx, int32_t *removed_out) {
     return 0;
 }
 
+static int bake_env_run_argv_checked(const char *const *argv) {
+    bake_process_result_t result = {0};
+    if (bake_proc_run_argv(argv, &result) != 0) {
+        return -1;
+    }
+
+    if (result.interrupted || result.term_signal || result.exit_code != 0) {
+        return -1;
+    }
+
+    return 0;
+}
+
+static int bake_env_install_bake3_wrapper(const bake_context_t *ctx) {
+    static const char *script_path = "/usr/local/bin/bake3";
+    static const char *script_content =
+        "#!/usr/bin/env bash\n"
+        "\n"
+        "exec $HOME/bake3/bake3 \"$@\"\n";
+
+    size_t current_len = 0;
+    char *current = bake_read_file(script_path, &current_len);
+    if (current && !strcmp(current, script_content)) {
+        ecs_os_free(current);
+        return 0;
+    }
+    ecs_os_free(current);
+
+    char *tmp_script = bake_join_path(ctx->bake_home, ".bake3-wrapper.sh");
+    if (!tmp_script) {
+        return -1;
+    }
+
+    if (bake_write_file(tmp_script, script_content) != 0) {
+        ecs_os_free(tmp_script);
+        return -1;
+    }
+
+    const char *chmod_argv[] = {"chmod", "+x", tmp_script, NULL};
+    if (bake_env_run_argv_checked(chmod_argv) != 0) {
+        remove(tmp_script);
+        ecs_os_free(tmp_script);
+        return -1;
+    }
+
+    const char *copy_argv[] = {"sudo", "cp", tmp_script, script_path, NULL};
+    int rc = bake_env_run_argv_checked(copy_argv);
+    remove(tmp_script);
+    ecs_os_free(tmp_script);
+    return rc;
+}
+
 int bake_environment_setup(bake_context_t *ctx, const char *argv0) {
-    char *bin = bake_join_path(ctx->bake_home, "bin");
-    if (!bin) {
+    if (bake_mkdirs(ctx->bake_home) != 0) {
         return -1;
     }
 
-    if (bake_mkdirs(bin) != 0) {
-        ecs_os_free(bin);
-        return -1;
-    }
-
-    const char *target_name = bake_os_executable_name();
-
-    char *dst = bake_join_path(bin, target_name);
-    ecs_os_free(bin);
+    char *dst = bake_join_path(ctx->bake_home, "bake3");
     if (!dst) {
         return -1;
     }
@@ -1064,9 +1107,19 @@ int bake_environment_setup(bake_context_t *ctx, const char *argv0) {
         return -1;
     }
 
-    int rc = bake_copy_file(src, dst);
+    int rc = 0;
+    if (!bake_path_equal_normalized(src, dst)) {
+        if (rename(src, dst) != 0) {
+            rc = bake_copy_file(src, dst);
+            if (rc == 0) {
+                rc = remove(src);
+            }
+        }
+    }
+
     if (rc == 0) {
         ecs_trace("installed bake to %s", dst);
+        rc = bake_env_install_bake3_wrapper(ctx);
     }
 
     ecs_os_free(src);
