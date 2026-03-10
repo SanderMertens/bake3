55622f37be474a079f2d5b26bdcc6ff685b7dcba
Sander Mertens <sander.mertens8@gmail.com>
2026-03-09 19:22:36 -0700

    Remove calls to signal()/setjmp()/jongjmp()

--- templates/test_harness/bake_test.c
+++ templates/test_harness/bake_test.c
@@ -2,8 +2,6 @@
 
 #include <errno.h>
 #include <math.h>
-#include <signal.h>
-#include <setjmp.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -17,9 +15,6 @@
 #endif
 
 #define BAKE_TEST_PARAM_MAX (128)
-#define BAKE_JMP_FAIL (1)
-#define BAKE_JMP_ABORT (2)
-#define BAKE_JMP_QUARANTINE (3)
 #define BAKE_TEST_EMPTY (2)
 #define BAKE_TEST_QUARANTINED (3)
 #define BAKE_COLOR_GREEN "\033[32m"
@@ -30,17 +25,10 @@
 static bake_test_suite *g_current_suite = NULL;
 static bake_test_case *g_current_case = NULL;
 static bool g_expect_abort = false;
-static bool g_observed_abort = false;
 static bool g_failed = false;
 static bool g_flaky = false;
-static const char *g_quarantine_date = NULL;
-static int g_jmp_active = 0;
-static jmp_buf g_jmp;
 static const char *g_cli_params[BAKE_TEST_PARAM_MAX];
 static int g_cli_param_count = 0;
-static volatile sig_atomic_t g_interrupted = 0;
-static void bake_abort_handler(int sig);
-static void bake_interrupt_handler(int sig);
 
 static bool bake_use_colors(void) {
     static int initialized = 0;
@@ -70,11 +58,6 @@ static void bake_print_status(const char *status, const char *color) {
     } else {
         fputs(status, stdout);
     }
-}
-
-static void bake_interrupt_handler(int sig) {
-    (void)sig;
-    g_interrupted = 1;
 }
 
 static const char* bake_debug_exec(const char *exec) {
@@ -260,11 +243,7 @@ static int bake_parse_cmd(const char *cmd, char ***argv_out, int *argc_out) {
     return 0;
 }
 
-static int bake_run_subprocess(const char *cmd, int *sig_out) {
-    if (sig_out) {
-        *sig_out = 0;
-    }
-
+static int bake_run_subprocess(const char *cmd) {
 #if defined(_WIN32)
     STARTUPINFOA si;
     PROCESS_INFORMATION pi;
@@ -283,19 +262,11 @@ static int bake_run_subprocess(const char *cmd, int *sig_out) {
         return -1;
     }
 
-    for (;;) {
-        DWORD wait_rc = WaitForSingleObject(pi.hProcess, 20);
-        if (wait_rc == WAIT_OBJECT_0) {
-            break;
-        }
-        if (wait_rc != WAIT_TIMEOUT) {
-            CloseHandle(pi.hThread);
-            CloseHandle(pi.hProcess);
-            return -1;
-        }
-        if (g_interrupted) {
-            TerminateProcess(pi.hProcess, 130);
-        }
+    DWORD wait_rc = WaitForSingleObject(pi.hProcess, INFINITE);
+    if (wait_rc != WAIT_OBJECT_0) {
+        CloseHandle(pi.hThread);
+        CloseHandle(pi.hProcess);
+        return -1;
     }
 
     DWORD exit_code = 0;
@@ -322,7 +293,6 @@ static int bake_run_subprocess(const char *cmd, int *sig_out) {
     }
 
     if (pid == 0) {
-        signal(SIGINT, SIG_DFL);
         execvp(argv[0], argv);
         _exit(127);
     }
@@ -331,17 +301,9 @@ static int bake_run_subprocess(const char *cmd, int *sig_out) {
 
     int status = 0;
     for (;;) {
-        if (g_interrupted) {
-            kill(pid, SIGINT);
-        }
-
-        pid_t rc = waitpid(pid, &status, WNOHANG);
+        pid_t rc = waitpid(pid, &status, 0);
         if (rc == pid) {
             break;
-        }
-        if (rc == 0) {
-            usleep(10000);
-            continue;
         }
         if (errno == EINTR) {
             continue;
@@ -349,12 +311,6 @@ static int bake_run_subprocess(const char *cmd, int *sig_out) {
         return -1;
     }
 
-    if (WIFSIGNALED(status)) {
-        if (sig_out) {
-            *sig_out = WTERMSIG(status);
-        }
-        return 128 + WTERMSIG(status);
-    }
     if (WIFEXITED(status)) {
         return WEXITSTATUS(status);
     }
@@ -362,7 +318,7 @@ static int bake_run_subprocess(const char *cmd, int *sig_out) {
 #endif
 }
 
-static void bake_set_failure(const char *file, int line, const char *msg, bool jump) {
+static void bake_set_failure(const char *file, int line, const char *msg) {
     g_failed = true;
     if (g_current_suite && g_current_case) {
         bake_print_status("FAIL", BAKE_COLOR_RED);
@@ -370,9 +326,6 @@ static void bake_set_failure(const char *file, int line, const char *msg, bool j
     } else {
         bake_print_status("FAIL", BAKE_COLOR_RED);
         printf(": %s:%d: %s\n", file, line, msg);
-    }
-    if (jump && g_jmp_active) {
-        longjmp(g_jmp, BAKE_JMP_FAIL);
     }
 }
 
@@ -403,38 +356,37 @@ const char* test_param(const char *name) {
     return bake_lookup_param(name);
 }
 
+static void bake_test_exit(void) {
+    exit(g_flaky ? 0 : -1);
+}
+
 void test_is_flaky(void) {
     g_flaky = true;
 }
 
 void test_quarantine(const char *date) {
-    g_quarantine_date = date ? date : "unknown";
-    if (g_jmp_active) {
-        longjmp(g_jmp, BAKE_JMP_QUARANTINE);
+    const char *quarantine_date = date ? date : "unknown";
+    bake_print_status("SKIP", BAKE_COLOR_YELLOW);
+    if (g_current_suite && g_current_case) {
+        printf(": %s.%s: test was quarantined on %s\n",
+            g_current_suite->id, g_current_case->id, quarantine_date);
+    } else {
+        printf(": test was quarantined on %s\n", quarantine_date);
     }
+    exit(BAKE_TEST_QUARANTINED);
 }
 
 void test_expect_abort(void) {
     g_expect_abort = true;
-    signal(SIGABRT, bake_abort_handler);
-}
-
-static void bake_abort_handler(int sig) {
-    (void)sig;
-    if (g_expect_abort) {
-        g_observed_abort = true;
-        if (g_jmp_active) {
-            longjmp(g_jmp, BAKE_JMP_ABORT);
-        }
-        exit(0);
-    }
-
-    bake_set_failure(__FILE__, __LINE__, "unexpected abort", true);
-    exit(-1);
 }
 
 void test_abort(void) {
-    bake_abort_handler(SIGABRT);
+    if (g_expect_abort) {
+        exit(0);
+    }
+
+    bake_set_failure(__FILE__, __LINE__, "unexpected abort");
+    bake_test_exit();
 }
 
 bool _if_test_assert(bool cond, const char *cond_str, const char *file, int line) {
@@ -444,7 +396,7 @@ bool _if_test_assert(bool cond, const char *cond_str, const char *file, int line
     if (!cond) {
         char msg[512];
         snprintf(msg, sizeof(msg), "assert(%s)", cond_str);
-        bake_set_failure(file, line, msg, false);
+        bake_set_failure(file, line, msg);
         return false;
     }
     return true;
@@ -455,7 +407,7 @@ bool _if_test_int(int64_t v1, int64_t v2, const char *str_v1, const char *str_v2
     if (v1 != v2) {
         char msg[512];
         snprintf(msg, sizeof(msg), "%s (%lld) != %s (%lld)", str_v1, (long long)v1, str_v2, (long long)v2);
-        bake_set_failure(file, line, msg, false);
+        bake_set_failure(file, line, msg);
         return false;
     }
     return true;
@@ -466,7 +418,7 @@ bool _if_test_uint(uint64_t v1, uint64_t v2, const char *str_v1, const char *str
     if (v1 != v2) {
         char msg[512];
         snprintf(msg, sizeof(msg), "%s (%llu) != %s (%llu)", str_v1, (unsigned long long)v1, str_v2, (unsigned long long)v2);
-        bake_set_failure(file, line, msg, false);
+        bake_set_failure(file, line, msg);
         return false;
     }
     return true;
@@ -477,7 +429,7 @@ bool _if_test_bool(bool v1, bool v2, const char *str_v1, const char *str_v2, con
     if (v1 != v2) {
         char msg[512];
         snprintf(msg, sizeof(msg), "%s (%s) != %s (%s)", str_v1, v1 ? "true" : "false", str_v2, v2 ? "true" : "false");
-        bake_set_failure(file, line, msg, false);
+        bake_set_failure(file, line, msg);
         return false;
     }
     return true;
@@ -489,7 +441,7 @@ bool _if_test_flt(double v1, double v2, const char *str_v1, const char *str_v2, 
     if (d > 0.000001) {
         char msg[512];
         snprintf(msg, sizeof(msg), "%s (%f) != %s (%f)", str_v1, v1, str_v2, v2);
-        bake_set_failure(file, line, msg, false);
+        bake_set_failure(file, line, msg);
         return false;
     }
     return true;
@@ -507,7 +459,7 @@ bool _if_test_str(const char *v1, const char *v2, const char *str_v1, const char
     if (!equal) {
         char msg[1024];
         snprintf(msg, sizeof(msg), "%s (%s) != %s (%s)", str_v1, v1 ? v1 : "NULL", str_v2, v2 ? v2 : "NULL");
-        bake_set_failure(file, line, msg, false);
+        bake_set_failure(file, line, msg);
         return false;
     }
 
@@ -519,7 +471,7 @@ bool _if_test_null(void *v, const char *str_v, const char *file, int line) {
     if (v != NULL) {
         char msg[512];
         snprintf(msg, sizeof(msg), "%s is not NULL", str_v);
-        bake_set_failure(file, line, msg, false);
+        bake_set_failure(file, line, msg);
         return false;
     }
     return true;
@@ -530,7 +482,7 @@ bool _if_test_not_null(void *v, const char *str_v, const char *file, int line) {
     if (v == NULL) {
         char msg[512];
         snprintf(msg, sizeof(msg), "%s is NULL", str_v);
-        bake_set_failure(file, line, msg, false);
+        bake_set_failure(file, line, msg);
         return false;
     }
     return true;
@@ -541,38 +493,38 @@ bool _if_test_ptr(const void *v1, const void *v2, const char *str_v1, const char
     if (v1 != v2) {
         char msg[512];
         snprintf(msg, sizeof(msg), "%s (%p) != %s (%p)", str_v1, v1, str_v2, v2);
-        bake_set_failure(file, line, msg, false);
+        bake_set_failure(file, line, msg);
         return false;
     }
     return true;
 }
 
 void _test_assert(bool cond, const char *cond_str, const char *file, int line) {
-    if (!_if_test_assert(cond, cond_str, file, line) && g_jmp_active) { longjmp(g_jmp, BAKE_JMP_FAIL); }
+    if (!_if_test_assert(cond, cond_str, file, line)) { bake_test_exit(); }
 }
 void _test_int(int64_t v1, int64_t v2, const char *str_v1, const char *str_v2, const char *file, int line) {
-    if (!_if_test_int(v1, v2, str_v1, str_v2, file, line) && g_jmp_active) { longjmp(g_jmp, BAKE_JMP_FAIL); }
+    if (!_if_test_int(v1, v2, str_v1, str_v2, file, line)) { bake_test_exit(); }
 }
 void _test_uint(uint64_t v1, uint64_t v2, const char *str_v1, const char *str_v2, const char *file, int line) {
-    if (!_if_test_uint(v1, v2, str_v1, str_v2, file, line) && g_jmp_active) { longjmp(g_jmp, BAKE_JMP_FAIL); }
+    if (!_if_test_uint(v1, v2, str_v1, str_v2, file, line)) { bake_test_exit(); }
 }
 void _test_bool(bool v1, bool v2, const char *str_v1, const char *str_v2, const char *file, int line) {
-    if (!_if_test_bool(v1, v2, str_v1, str_v2, file, line) && g_jmp_active) { longjmp(g_jmp, BAKE_JMP_FAIL); }
+    if (!_if_test_bool(v1, v2, str_v1, str_v2, file, line)) { bake_test_exit(); }
 }
 void _test_flt(double v1, double v2, const char *str_v1, const char *str_v2, const char *file, int line) {
-    if (!_if_test_flt(v1, v2, str_v1, str_v2, file, line) && g_jmp_active) { longjmp(g_jmp, BAKE_JMP_FAIL); }
+    if (!_if_test_flt(v1, v2, str_v1, str_v2, file, line)) { bake_test_exit(); }
 }
 void _test_str(const char *v1, const char *v2, const char *str_v1, const char *str_v2, const char *file, int line) {
-    if (!_if_test_str(v1, v2, str_v1, str_v2, file, line) && g_jmp_active) { longjmp(g_jmp, BAKE_JMP_FAIL); }
+    if (!_if_test_str(v1, v2, str_v1, str_v2, file, line)) { bake_test_exit(); }
 }
 void _test_null(void *v, const char *str_v, const char *file, int line) {
-    if (!_if_test_null(v, str_v, file, line) && g_jmp_active) { longjmp(g_jmp, BAKE_JMP_FAIL); }
+    if (!_if_test_null(v, str_v, file, line)) { bake_test_exit(); }
 }
 void _test_not_null(void *v, const char *str_v, const char *file, int line) {
-    if (!_if_test_not_null(v, str_v, file, line) && g_jmp_active) { longjmp(g_jmp, BAKE_JMP_FAIL); }
+    if (!_if_test_not_null(v, str_v, file, line)) { bake_test_exit(); }
 }
 void _test_ptr(const void *v1, const void *v2, const char *str_v1, const char *str_v2, const char *file, int line) {
-    if (!_if_test_ptr(v1, v2, str_v1, str_v2, file, line) && g_jmp_active) { longjmp(g_jmp, BAKE_JMP_FAIL); }
+    if (!_if_test_ptr(v1, v2, str_v1, str_v2, file, line)) { bake_test_exit(); }
 }
 
 static int bake_run_case(bake_test_suite *suite, bake_test_case *tc) {
@@ -580,36 +532,21 @@ static int bake_run_case(bake_test_suite *suite, bake_test_case *tc) {
     g_current_case = tc;
     suite->assert_count = 0;
     g_expect_abort = false;
-    g_observed_abort = false;
     g_failed = false;
     g_flaky = false;
-    g_quarantine_date = NULL;
-    signal(SIGABRT, bake_abort_handler);
 
     if (suite->setup) {
         suite->setup();
     }
 
-    g_jmp_active = 1;
-    int jmp_rc = setjmp(g_jmp);
-    if (jmp_rc == 0) {
-        tc->function();
-    }
-    g_jmp_active = 0;
-    signal(SIGABRT, SIG_DFL);
+    tc->function();
 
     if (suite->teardown) {
         suite->teardown();
     }
 
-    if (g_quarantine_date) {
-        bake_print_status("SKIP", BAKE_COLOR_YELLOW);
-        printf(": %s.%s: test was quarantined on %s\n", suite->id, tc->id, g_quarantine_date);
-        return BAKE_TEST_QUARANTINED;
-    }
-
-    if (g_expect_abort && !g_observed_abort) {
-        bake_set_failure(__FILE__, __LINE__, "expected abort signal", false);
+    if (g_expect_abort) {
+        bake_set_failure(__FILE__, __LINE__, "expected abort");
     }
 
     if (g_failed) {
@@ -640,10 +577,6 @@ static int bake_run_suite(const char *test_id, const char *exec, bake_test_suite
     int fail = 0;
     int empty = 0;
     for (uint32_t i = 0; i < suite->testcase_count; i++) {
-        if (g_interrupted) {
-            return -2;
-        }
-
         char cmd[4096];
         int written = snprintf(cmd, sizeof(cmd), "\"%s\" \"%s.%s\"", exec, suite->id, suite->testcases[i].id);
         if (written < 0 || (size_t)written >= sizeof(cmd)) {
@@ -680,12 +613,7 @@ static int bake_run_suite(const char *test_id, const char *exec, bake_test_suite
             strcat(cmd, value);
         }
 
-        int sig = 0;
-        int test_rc = bake_run_subprocess(cmd, &sig);
-        if (g_interrupted || sig == SIGINT || test_rc == 130) {
-            g_interrupted = 1;
-            return -2;
-        }
+        int test_rc = bake_run_subprocess(cmd);
         if (test_rc == 0) {
             pass ++;
             continue;
@@ -703,10 +631,6 @@ static int bake_run_suite(const char *test_id, const char *exec, bake_test_suite
             continue;
         }
 
-        if (sig) {
-            bake_print_status("FAIL", BAKE_COLOR_RED);
-            printf(": %s.%s exited with signal %d\n", suite->id, suite->testcases[i].id, sig);
-        }
         fail ++;
         rc = -1;
         bake_print_debug_command(exec, suite, &suite->testcases[i]);
@@ -741,9 +665,6 @@ static int bake_run_suite_for_params(const char *test_id, const char *exec, bake
     for (int32_t i = 0; i < p->value_count; i++) {
         p->value_cur = i;
         int suite_rc = bake_run_suite_for_params(test_id, exec, suite, param + 1, pass, fail, empty);
-        if (suite_rc == -2) {
-            return -2;
-        }
         if (suite_rc != 0) {
             rc = -1;
         }
@@ -768,10 +689,6 @@ typedef struct bake_suite_jobs_t {
 static void* bake_suite_worker(void *arg) {
     bake_suite_jobs_t *jobs = arg;
     for (;;) {
-        if (g_interrupted) {
-            return NULL;
-        }
-
         pthread_mutex_lock(&jobs->lock);
         uint32_t suite_index = jobs->next_suite;
         if (suite_index < jobs->suite_count) {
@@ -792,9 +709,7 @@ static void* bake_suite_worker(void *arg) {
         jobs->pass += pass;
         jobs->fail += fail;
         jobs->empty += empty;
-        if (suite_rc == -2) {
-            g_interrupted = 1;
-        } else if (suite_rc != 0) {
+        if (suite_rc != 0) {
             jobs->rc = -1;
         }
         pthread_mutex_unlock(&jobs->lock);
@@ -921,24 +836,19 @@ int bake_test_run(const char *test_id, int argc, char *argv[], bake_test_suite *
     int job_count = 1;
 
     g_cli_param_count = 0;
-    g_interrupted = 0;
-    void (*prev_sigint)(int) = signal(SIGINT, bake_interrupt_handler);
 
     for (int i = 1; i < argc; i++) {
         const char *arg = argv[i];
         if (!strcmp(arg, "--list-tests")) {
             bake_list_tests(suites, suite_count);
-            signal(SIGINT, prev_sigint);
             return 0;
         }
         if (!strcmp(arg, "--list-suites")) {
             bake_list_suites(suites, suite_count);
-            signal(SIGINT, prev_sigint);
             return 0;
         }
         if (!strcmp(arg, "--list-commands")) {
             bake_list_commands(argv[0], suites, suite_count);
-            signal(SIGINT, prev_sigint);
             return 0;
         }
         if (!strcmp(arg, "--param")) {
@@ -950,7 +860,6 @@ int bake_test_run(const char *test_id, int argc, char *argv[], bake_test_suite *
                 continue;
             }
             printf("invalid --param argument\n");
-            signal(SIGINT, prev_sigint);
             return -1;
         }
         if (!strcmp(arg, "-j")) {
@@ -963,7 +872,6 @@ int bake_test_run(const char *test_id, int argc, char *argv[], bake_test_suite *
                 continue;
             }
             printf("missing value for -j\n");
-            signal(SIGINT, prev_sigint);
             return -1;
         }
 
@@ -975,9 +883,7 @@ int bake_test_run(const char *test_id, int argc, char *argv[], bake_test_suite *
     }
 
     if (single_test) {
-        int run_rc = bake_run_single_test(suites, suite_count, single_test);
-        signal(SIGINT, prev_sigint);
-        return run_rc;
+        return bake_run_single_test(suites, suite_count, single_test);
     }
 
     int pass = 0;
@@ -989,13 +895,10 @@ int bake_test_run(const char *test_id, int argc, char *argv[], bake_test_suite *
         bake_test_suite *suite = bake_find_suite(suites, suite_count, suite_filter);
         if (!suite) {
             printf("test suite '%s' not found\n", suite_filter);
-            signal(SIGINT, prev_sigint);
             return -1;
         }
         int suite_rc = bake_run_suite_for_params(test_id, argv[0], suite, 0, &pass, &fail, &empty);
-        if (suite_rc == -2) {
-            g_interrupted = 1;
-        } else if (suite_rc != 0) {
+        if (suite_rc != 0) {
             rc = -1;
         }
     } else {
@@ -1009,26 +912,13 @@ int bake_test_run(const char *test_id, int argc, char *argv[], bake_test_suite *
         {
             for (uint32_t s = 0; s < suite_count; s++) {
                 int suite_rc = bake_run_suite_for_params(test_id, argv[0], &suites[s], 0, &pass, &fail, &empty);
-                if (suite_rc == -2) {
-                    g_interrupted = 1;
-                    break;
-                }
                 if (suite_rc != 0) {
                     rc = -1;
                 }
             }
         }
-        if (!g_interrupted) {
-            printf("-----------------------------\n");
-            bake_print_report(test_id, "all", "", pass, fail, empty);
-        }
-    }
-
-    signal(SIGINT, prev_sigint);
-    if (g_interrupted) {
-        signal(SIGINT, SIG_DFL);
-        raise(SIGINT);
-        return -1;
+        printf("-----------------------------\n");
+        bake_print_report(test_id, "all", "", pass, fail, empty);
     }
 
     return rc;
