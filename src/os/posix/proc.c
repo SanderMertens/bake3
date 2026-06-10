dbe6585ea5d01eb3cba4f113a771a2d7067b3122
Sander Mertens <sander.mertens8@gmail.com>
2026-06-10 16:36:19 -0700

    Use posix_spawn instead of fork to safely spawn commands from worker threads

--- src/os/posix/proc.c
+++ src/os/posix/proc.c
@@ -6,29 +6,15 @@
 #include <errno.h>
 #include <fcntl.h>
 #include <signal.h>
+#include <spawn.h>
 #include <sys/wait.h>
 #include <unistd.h>
 
-static int bake_proc_redirect_file(const char *path, int fd, int flags) {
-    int file = open(path, flags, 0644);
-    if (file < 0) {
-        bake_log_errno_last("open redirected stdio file", path);
-        return -1;
-    }
+extern char **environ;
 
-    if (dup2(file, fd) < 0) {
-        bake_log_errno_last("redirect stdio", path);
-        close(file);
-        return -1;
-    }
-
-    if (close(file) != 0) {
-        bake_log_errno_last("close redirected stdio file", path);
-        return -1;
-    }
-    return 0;
-}
-
+/* posix_spawn instead of fork/exec: bake spawns compilers from worker
+ * threads, and code between fork and exec in a multithreaded process is
+ * limited to async-signal-safe calls. */
 int bake_proc_run(
     const char *const *argv,
     const bake_process_stdio_t *stdio_cfg,
@@ -38,46 +24,64 @@ int bake_proc_run(
         return -1;
     }
 
-    pid_t pid = fork();
-    if (pid < 0) {
+    posix_spawn_file_actions_t fa;
+    posix_spawnattr_t attr;
+    if (posix_spawn_file_actions_init(&fa) != 0) {
+        return -1;
+    }
+    if (posix_spawnattr_init(&attr) != 0) {
+        posix_spawn_file_actions_destroy(&fa);
         return -1;
     }
 
-    if (pid == 0) {
-        signal(SIGINT, SIG_DFL);
-
-        if (stdio_cfg) {
-            if (stdio_cfg->stdin_path && stdio_cfg->stdin_path[0]) {
-                if (bake_proc_redirect_file(stdio_cfg->stdin_path, STDIN_FILENO, O_RDONLY) != 0) {
-                    _exit(127);
-                }
-            }
-
-            if (stdio_cfg->stdout_path && stdio_cfg->stdout_path[0]) {
-                int flags = O_WRONLY | O_CREAT |
-                    (stdio_cfg->stdout_append ? O_APPEND : O_TRUNC);
-                if (bake_proc_redirect_file(stdio_cfg->stdout_path, STDOUT_FILENO, flags) != 0) {
-                    _exit(127);
-                }
-            }
-
-            if (stdio_cfg->stderr_path && stdio_cfg->stderr_path[0]) {
-                int flags = O_WRONLY | O_CREAT |
-                    (stdio_cfg->stderr_append ? O_APPEND : O_TRUNC);
-                if (bake_proc_redirect_file(stdio_cfg->stderr_path, STDERR_FILENO, flags) != 0) {
-                    _exit(127);
-                }
-            }
-
-            if (stdio_cfg->stderr_to_stdout) {
-                if (dup2(STDOUT_FILENO, STDERR_FILENO) < 0) {
-                    _exit(127);
-                }
-            }
+    int err = 0;
+    if (stdio_cfg) {
+        if (stdio_cfg->stdin_path && stdio_cfg->stdin_path[0]) {
+            err = posix_spawn_file_actions_addopen(
+                &fa, STDIN_FILENO, stdio_cfg->stdin_path, O_RDONLY, 0);
         }
 
-        execvp(argv[0], (char *const*)argv);
-        _exit(127);
+        if (!err && stdio_cfg->stdout_path && stdio_cfg->stdout_path[0]) {
+            int flags = O_WRONLY | O_CREAT |
+                (stdio_cfg->stdout_append ? O_APPEND : O_TRUNC);
+            err = posix_spawn_file_actions_addopen(
+                &fa, STDOUT_FILENO, stdio_cfg->stdout_path, flags, 0644);
+        }
+
+        if (!err && stdio_cfg->stderr_path && stdio_cfg->stderr_path[0]) {
+            int flags = O_WRONLY | O_CREAT |
+                (stdio_cfg->stderr_append ? O_APPEND : O_TRUNC);
+            err = posix_spawn_file_actions_addopen(
+                &fa, STDERR_FILENO, stdio_cfg->stderr_path, flags, 0644);
+        }
+
+        if (!err && stdio_cfg->stderr_to_stdout) {
+            err = posix_spawn_file_actions_adddup2(
+                &fa, STDOUT_FILENO, STDERR_FILENO);
+        }
+    }
+
+    sigset_t sig_default;
+    sigemptyset(&sig_default);
+    sigaddset(&sig_default, SIGINT);
+    if (!err) {
+        err = posix_spawnattr_setsigdefault(&attr, &sig_default);
+    }
+    if (!err) {
+        err = posix_spawnattr_setflags(&attr, POSIX_SPAWN_SETSIGDEF);
+    }
+
+    pid_t pid = 0;
+    if (!err) {
+        err = posix_spawnp(&pid, argv[0], &fa, &attr, (char *const*)argv, environ);
+    }
+
+    posix_spawn_file_actions_destroy(&fa);
+    posix_spawnattr_destroy(&attr);
+
+    if (err) {
+        bake_log_errno("start command", argv[0], err);
+        return -1;
     }
 
     int status = 0;
@@ -91,6 +95,7 @@ int bake_proc_run(
             continue;
         }
 
+        bake_log_errno_last("wait for command", argv[0]);
         return -1;
     }
 
